8d7c4a4fdf54ef2ab981fc6a3d79a0f88bdb4b27
Aditya <pandey.api2k@gmail.com>
2021-08-09 12:42:10 +0530

    Added Dynamic Navbar, That changes according to the type of user login in

--- assets/components/navbar.inc.php
+++ assets/components/navbar.inc.php
@@ -6,6 +6,39 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <?php 
+
+      if(isset($_SESSION['standard']) && isset($_SESSION['rollno'])){
+        //Navbar for student
+        ?>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="studentProfile.php">Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="studentResult.php">Results</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="../src/student/studentLogout.php">Logout</a>
+        </li>
+        <?php
+      }
+      elseif(isset($_SESSION['adminEmail'])){
+        //Navbar for Admin
+        ?>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="adminLogin.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="adminChangePassword.php">Change Password</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="../src/admin/adminLogout.php">Logout</a>
+        </li>
+        <?php
+      }
+      else{
+        //Navbar for visitor
+        ?>
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="index.php">Home</a>
         </li>
@@ -15,7 +48,9 @@
         <li class="nav-item">
           <a class="nav-link" href="adminLogin.php">Admin Login</a>
         </li>
-
+        <?php
+      }
+      ?>
       </ul>
     </div>
   </div>
